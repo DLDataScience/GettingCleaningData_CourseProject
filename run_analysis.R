e7b300c0b57da037dfbf281b6ec2a216b460728b
DLDataScience <dlevy93@gmail.com>
2020-11-15 11:40:28 -0800

    Added early draft of all submission files.

---
+++ run_analysis.R
@@ -0,0 +1,20 @@
+### Getting & Cleaning Data — Course Project
+### David Levy
+### November 15, 2020
+
+##   Per assignment directions, the run_analysis.R script should do the 
+##   following:
+
+##   1. Merges the training and the test sets to create one data set.
+
+mergedData <- merge()
+
+##   2. Extracts only the measurements on the mean and standard deviation for 
+##      each measurement.
+
+##   3. Uses descriptive activity names to name the activities in the data set
+
+##   4. Appropriately labels the data set with descriptive variable names.
+
+##   5. From the data set in step 4, creates a second, independent tidy data set 
+##      with the average of each variable for each activity and each subject.
