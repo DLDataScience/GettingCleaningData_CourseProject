7a6e83c1075e61ff25b555cf055c163f748a59b9
DLDataScience <dlevy93@gmail.com>
2020-11-15 22:54:20 -0800

    README.md and CodeBook.md files written. Slight adjustments to variable names in run_analysis.R to avoid confusion with output data. Wrote tidy data sets to .csv files in new Tidy Data directory.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# GettingCleaningData_CourseProject
+## Getting and Cleaning Data — Course Project
+##### David Levy
+
+
+
+### UCI HAR Dataset
+
+The `UCI HAR Dataset` directory is comprised of the unzipped contents of the `harus.zip` file downloaded directly from the UCI Machine Learning repository at `https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip`. The dataset is formally entitled **Human Activity Recognition Using Smartphones Data Set**, and pertains to "*Human Activity Recognition database built from the recordings of 30 subjects performing activities of daily living (ADL) while carrying a waist-mounted smartphone with embedded inertial sensors*," as described by the Abstract on the UCI Machine Learning Repository landing page for the dataset.
+
+This repository contains its own `README.txt` file that should be referred to for details on the format in which the data were originally retrieved for this analysis.
+
+
+
+### CodeBook.md
+
+The `CodeBook.md` file is adapted from the `README.txt` and `features_info.txt` provided in the `UCI HAR Dataset` directory. It describes all variables that were selected and the manner in which they were transformed during the tidying process executed by the `run_analysis.R` script. All transformations are also heavily documented in the script itself.
+
+All data and Code Book descriptions pertaining to data that was not selected for the final tidy datasets in this analysis have been removed from the `CodeBook.md` file. The original descriptions remain intact as they were retreieved from the UCI Machine Learning Repository in the documentation located in the `UCI HAR Dataset` directory.
+
+
+
+### run_analysis.R
+
+The `run_analysis.R` script downloads all necesssary data files, loads all R package dependencies, and executes five basic tasks:
+
+1. Merges the training and the test sets to create one data set.
+
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+
+3. Uses descriptive activity names to name the activities in the data set
+
+4. Appropriately labels the data set with descriptive variable names.
+
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
--- run_analysis.R
+++ run_analysis.R
@@ -2,10 +2,29 @@
 ### David Levy
 ### November 15, 2020
 
-##   Per assignment directions, the run_analysis.R script should do the 
-##   following:
+##  load libraries and retrieve data
+library(tidyverse)
 
-##   1. Merges the training and the test sets to create one data set.
+# data loading string initiation
+datafile <- 'harus.zip'
+url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+dir <- 'UCI HAR Dataset'
+
+# file download if not already complete
+if(!file.exists(datafile)){
+    download.file(url, datafile) 
+}
+
+# unzip file if not already complete
+if(!file.exists(dir)){
+    unzip(datafile)
+}
+
+
+##  per assignment directions, the run_analysis.R script should do the 
+##  following:
+
+##  1. Merges the training and the test sets to create one data set.
 
 # retrieve test data
 xtest <- read.table('UCI HAR Dataset/test/X_test.txt')
@@ -25,8 +44,12 @@ subj <- rbind(subjtest, subjtrain)
 
 
 
-##   2. Extracts only the measurements on the mean and standard deviation for 
-##      each measurement.
+##  2. Extracts only the measurements on the mean and standard deviation for 
+##     each measurement.
+
+# get features from the appropriate file
+features <- read_delim('UCI HAR Dataset/features.txt', ' ',
+                       col_names = c('index', 'feature'))
 
 # construct a T/F vector marking the indices of desired variables
 mean_std_ind <- grepl('(mean|std)', features$feature)
@@ -38,21 +61,53 @@ x_red <- x[, mean_std_ind]
 
 
 
-##   3. Uses descriptive activity names to name the activities in the data set
+##  3. Uses descriptive activity names to name the activities in the data set
+
+# get activity labels from the appropriate file
+activityLabels <- read_delim('UCI HAR Dataset/activity_labels.txt', ' ',
+                             col_names = c('label', 'activity'))
+
+# reformat labels to meet tidy data guidelines
+actLabs <- str_to_lower(str_remove_all(activityLabels$activity, 
+                                                       '[^[:alnum:]]'))
 
 # turn numeric y data into a factor with labels from activity_labels.txt
-y$activity <- factor(y$V1, labels=activityLabels$activity)
+activity <- factor(y$V1, labels=actLabs)
 
 
 
 
-##   4. Appropriately labels the data set with descriptive variable names.
+##  4. Appropriately labels the data set with descriptive variable names.
 
 # retrieve column names for the reduced x_red dataframe
 names(x_red) <- features$feature[mean_std_ind]
 
+# transform subject data into factors and rename column
+subject <- as.factor(subj$V1)
+
+# column bind activity and subject data to x_red with descriptive names
+tidyDataSet <- cbind(subject, x_red, activity)
 
 
 
-##   5. From the data set in step 4, creates a second, independent tidy data set 
-##      with the average of each variable for each activity and each subject.
+##  5. From the data set in step 4, creates a second, independent tidy data set 
+##     with the average of each variable for each activity and each subject.
+
+# use dplyr grouping and summarise across functions to get means within activity 
+# and subject gorupings
+avTidyData <- tidyDataSet %>%
+    group_by(activity, subject) %>%
+    summarise(across(everything(), mean))
+
+# rename all columns except for `subject` and `activity`
+names(avTidyData)[-c(1:2)] <- paste('avg_', names(avTidyData)[-c(1:2)], sep='')
+
+
+# write tidy data to new csv files
+outputFile <- 'Tidy Data'
+if(!file.exists(outputFile)){
+    dir.create(outputFile)
+}
+
+write.csv(tidyDataSet, 'Tidy Data/tidyDataSet.csv')
+write.csv(tidyDataSet, 'Tidy Data/avTidyData.csv')
